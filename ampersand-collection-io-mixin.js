65f8255de1206de9ef34906dcec877cccd7c3222
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-12-22 06:26:10 +0000

    independent socket.io wrapper module JGAntunes/ampersand-io

--- ampersand-collection-io-mixin.js
+++ ampersand-collection-io-mixin.js
@@ -1,7 +1,8 @@
 var extend = require('extend-object');
-var io = require('socket.io-client');
+var AmpersandExtend = require('ampersand-class-extend');
+var AmpersandIO = require('ampersand-io');
 
-var IOMixin = {
+var AmpersandCollectionIO = AmpersandIO.extend({
 
   // Fetch the default set of models for this collection, resetting the
   // collection when they arrive. If `reset: true` is passed, the response
@@ -113,88 +114,9 @@ var IOMixin = {
         }
       }
     });
-  },
+  }
 
-  socket: io('http://localhost:3000'),
-
-  // The name of the events to be used in each operation
-  events: {
-    onNew: 'on-model-new',
-    onUpdate: 'on-model-update',
-    fetch: 'collection-fetch',
-    onFetch: 'fetch-response'
-  },
-
-  listeners: {
-    onUpdate:{ 
-      fn: function(data, cb){
-        var model = this.get(data.id);
-        model.save(data, null);
-        return cb();
-      },
-      active: false,
-    },
-    onNew: {
-      fn: function(data, cb){
-        this.create(data,{});
-        return cb();
-      },
-      active: false,
-    }
-  },
-
-  addListeners: function(){
-    for(var i = 0; i < arguments.length; i++){
-      var l = arguments[i];
-      if(this.listeners[l.listener] && this.listeners[l.listener].active){
-        console.log('listener already active');
-        continue;
-      }
-      if(l.listener && l.fn && typeof(l.fn) == 'function'){
-        this.listeners[l.listener] = {fn: l.fn};
-      }
-      if(l.active){
-        this.socket.on(l.listener, this.listeners[l.listener].fn);
-        this.listeners[l.listener].active = true;
-      }
-      else{
-        this.listeners[l.listener].active = false;
-      }
-    }
-  },
-
-  setListeners: function (listeners){
-    if(!listeners){
-      listeners = Object.keys(this.listeners);
-    }
-    for(var i = 0; i < listeners.length; i++){
-      var listener = listeners[i];
-      if(!this.listeners[listener].active){
-        this.socket.on(listener, this.listeners[listener].fn);
-        this.listeners[listener].active = true;
-      }
-    }
-  },
-
-  removeListeners: function (listeners){
-    if(!listeners){
-      listeners = Object.keys(this.listeners);
-    }
-    for(var i = 0; i < listeners.length; i++){
-      var listener = listeners[i];
-      console.log(listener);
-      if(this.listeners[listener].active){
-        this.socket.removeListener(listener, this.listeners[listener].fn);
-        this.listeners[listener].active = false;
-      }
-    }
-  },
-  // Overridable function responsible for emitting the events
-  emit: function (event, model, options){
-    this.socket.emit(event, model, options.callback);
-  },
-
-};
+});
 
 // Aux func used to trigger errors if they exist and use the optional
 // callback function if given
@@ -207,9 +129,4 @@ var callback = function(err, model, resp, options){
   }
 };
 
-var IOCollection = function(){
-};
-
-extend(IOCollection.prototype, IOMixin);
-
-module.exports = IOCollection;
+module.exports = AmpersandCollectionIO;
